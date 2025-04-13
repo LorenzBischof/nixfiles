1886649b0a24ecc2002f41376e0c41e169726b0e
LorenzBischof <1837725+LorenzBischof@users.noreply.github.com>
2025-04-13 17:01:04 +0200

    nix: fix gc syntax

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -197,7 +197,7 @@
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 1w";
+      options = "--delete-older-than 14d";
     };
   };
 
--- hosts/nas/configuration.nix
+++ hosts/nas/configuration.nix
@@ -238,7 +238,7 @@ in
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 1w";
+      options = "--delete-older-than 14d";
     };
   };
 
