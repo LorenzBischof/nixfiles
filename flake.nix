11dd155d12e4a1a8b868cea8c18816d0c0cf4531
LorenzBischof <1837725+LorenzBischof@users.noreply.github.com>
2025-07-09 22:12:46 +0200

    citrix: allow insecure package versions

--- flake.nix
+++ flake.nix
@@ -85,6 +85,9 @@
             "aspnetcore-runtime-wrapped-6.0.36"
             "dotnet-sdk-6.0.428"
             "dotnet-sdk-wrapped-6.0.428"
+            # Citrix Workspace
+            "libsoup-2.74.3"
+            "libxml2-2.13.8"
           ];
         };
         overlays = [
