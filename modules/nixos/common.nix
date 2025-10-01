bba4c3904a1b0c1c754a464a9b6f0097841f8413
LorenzBischof <1837725+LorenzBischof@users.noreply.github.com>
2025-10-01 22:48:18 +0200

    nix: disable community cache

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -45,11 +45,9 @@
         "@wheel"
       ];
       substituters = [
-        "https://nix-community.cachix.org?priority=41"
         "https://cache.lorenzbischof.com/system?priority=42"
       ];
       trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "system:DAJL6xmsmoUmZOeGL8XxrEWF5pdtFGMW2+cOGyYaqgU="
       ];
       netrc-file = config.age.secrets.netrc.path;
